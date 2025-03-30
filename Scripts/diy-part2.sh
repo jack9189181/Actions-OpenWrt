0ce60c9afe8dc9630653ec7c5fbb784335454843
jack9189181 <9189181@gmail.com>
2025-03-30 17:38:51 +0800

    #

--- Scripts/diy-part2.sh
+++ Scripts/diy-part2.sh
@@ -65,3 +65,22 @@ if [ -f "$NSS_PBUF" ]; then
 else
 	echo "err"
 fi
+
+PKG_PATH="$GITHUB_WORKSPACE/openwrt/"
+cd $PKG_PATH
+#高通平台调整
+DTS_PATH="./target/linux/qualcommax/files/arch/arm64/boot/dts/qcom/"
+if [[ $WRT_TARGET == *"QUALCOMMAX"* ]]; then
+	#取消nss相关feed
+	echo "CONFIG_FEED_nss_packages=n" >> ./.config
+	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
+	#设置NSS版本
+	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
+	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
+fi
+
+#编译器优化
+if [[ $WRT_TARGET != *"X86"* ]]; then
+	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
+	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+fi
