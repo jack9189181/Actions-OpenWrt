0c186775308f101a4a8cfb44cd4a0a49d139cfba
jack9189181 <9189181@gmail.com>
2025-04-13 14:19:36 +0800

    #

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -50,11 +50,11 @@ UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 
 # UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 # UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
-UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
+# UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 # UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
 # UPDATE_PACKAGE "ssr-plus" "fw876/helloworld" "master"
-UPDATE_PACKAGE "istore" "linkease/istore" "main"
-
+# UPDATE_PACKAGE "istore" "linkease/istore" "main"
+UPDATE_PACKAGE "lucky" "sirpdboy/luci-app-lucky" "main"
 # UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 
 # UPDATE_PACKAGE "alist" "sbwml/luci-app-alist" "main"
